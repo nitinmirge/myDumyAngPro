a8a999458a3afe435aecd69fab100123da9d8cb6
nitinmirge <nitinmirge123@gmail.com>
2023-07-31 13:26:21 +0530

    module created and its routing done,comman servise created

---
+++ DumyHotelPro/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes =
+[
+  {path:'',component:HomeComponent},
+
+  {path:'admin',loadChildren:()=> import('./admin/admin.module').then(mod => mod.AdminModule)},
+
+  {path:'owner',loadChildren:()=> import('./owner/owner.module').then(mod => mod.OwnerModule)},
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ DumyHotelPro/src/app/owner/owner-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OwnerhomeComponent } from './ownerhome/ownerhome.component';
+
+const routes: Routes =
+[
+  {path:'',component:OwnerhomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class OwnerRoutingModule { }
